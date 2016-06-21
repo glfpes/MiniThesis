253a51d7aa108aeebb492564773c28aac4568fd6
Guang Yang <glfpes@gmail.com>
2016-06-21 18:26:39 +0800

    0621

--- img.origin/source/fig13_frequency.m
+++ img.origin/source/fig13_frequency.m
@@ -3,16 +3,19 @@ y_4=25*rand(25,1)
 y_6=22*rand(25,1)
 y_8=26*rand(25,1)
 
-h_2 = cdfplot(y_2);
-set(h_2, 'Color', 'red');
-hold on;
-h_4 = cdfplot(y_4);
-set(h_4, 'Color', 'blue');
-hold on;
-h_6 = cdfplot(y_6);
-set(h_6, 'Color', 'black');
-h_8 = cdfplot(y_8);
-set(h_8, 'Color', 'green');
+[f,x] = ecdf(y_2);
+plot(x,f,'red');
+hold on
+[f,x] = ecdf(y_4);
+plot(x,f,'blue');
+hold on
+[f,x] = ecdf(y_6);
+plot(x,f,'black');
+hold on
+[f,x] = ecdf(y_8);
+plot(x,f,'green');
+hold on
+grid on;
 
 xlabel('Error(cm)');
 ylabel('CDF');
--- img.origin/source/fig13_light.m
+++ img.origin/source/fig13_light.m
@@ -1,10 +1,12 @@
 without=[3,9,17,11,15,23,26,31,18,16,16,19,20,30,16,9,11,13,17,10,22,12,16,14,12];
 with=[4,7,9,4,2,6,8,8,11,13,14,23,21,22,26,28,30,24,27,20,15,17,14,21,22];
-h_2 = cdfplot(without);
-set(h_2, 'Color', 'red');
-hold on;
-h_4 = cdfplot(with);
-set(h_4, 'Color', 'blue');
+[f,x] = ecdf(y_2);
+plot(x,f,'red');
+hold on
+[f,x] = ecdf(y_4);
+plot(x,f,'blue');
+hold on
+grid on;
 
 xlabel('Error(cm)');
 ylabel('CDF');
